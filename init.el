c2d9066b731e372a78e9b7c64595c2d86c74512d
Norgat <norg113@gmail.com>
2016-02-01 16:49:47 +0300

    Add base JavaScript configuration

--- init.el
+++ init.el
@@ -95,3 +95,17 @@
 
 (global-company-mode t)
 (global-set-key (kbd "<f1>") 'company-complete)
+
+
+
+;; ===========================================================================
+;; JavaScript configuration
+
+;; js2-mode used
+
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+(setq js2-highlight-level 3)
+
+(define-key js-mode-map "{" 'paredit-open-curly)
+(define-key js-mode-map "}" 'paredit-close-curly-and-newline)
