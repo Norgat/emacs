69b248877f49ae7665d8911bd709c425add443ce
Norgat <norg113@gmail.com>
2016-02-12 21:28:51 +0300

    Added support of russian layout to hotkeys. Fix hotkey for MS NEK 4000.

--- init.el
+++ init.el
@@ -11,6 +11,35 @@
 (package-initialize)
 
 
+;; ==========================================================================
+;; For hotkeys in russian layout
+
+(defun cfg:reverse-input-method (input-method)
+  "Build the reverse mapping of single letters from INPUT-METHOD."
+  (interactive
+   (list (read-input-method-name "Use input method (default current): ")))
+  (if (and input-method (symbolp input-method))
+      (setq input-method (symbol-name input-method)))
+  (let ((current current-input-method)
+        (modifiers '(nil (control) (meta) (control meta))))
+    (when input-method
+      (activate-input-method input-method))
+    (when (and current-input-method quail-keyboard-layout)
+      (dolist (map (cdr (quail-map)))
+        (let* ((to (car map))
+               (from (quail-get-translation
+                      (cadr map) (char-to-string to) 1)))
+          (when (and (characterp from) (characterp to))
+            (dolist (mod modifiers)
+              (define-key local-function-key-map
+                (vector (append mod (list from)))
+                (vector (append mod (list to)))))))))
+    (when input-method
+      (activate-input-method current))))
+
+(cfg:reverse-input-method 'russian-computer)
+
+
 
 ;; ===========================================================================
 ;; Common typing and control configuration
@@ -64,8 +93,8 @@
 (global-set-key (kbd "C-x <down>") 'windmove-down)
 (global-set-key (kbd "C-x <right>") 'windmove-right)
 (global-set-key (kbd "C-x <left>") 'windmove-left)
-(global-set-key (kbd "<f12>") 'ibuffer)
-;(global-set-key (kbd "<f12>") 'buffer-menu)
+(global-set-key (kbd "<f6>") 'ibuffer)
+;(Global-set-key (kbd "<f12>") 'buffer-menu)
 
 ;; org-mode global keys
 (global-set-key "\C-cl" 'org-store-link)
@@ -147,3 +176,6 @@
 (define-key js-mode-map "{" 'paredit-open-curly)
 (define-key js-mode-map "}" 'paredit-close-curly-and-newline)
 
+
+;; ===========================================================================
+;; Activate russian layout
